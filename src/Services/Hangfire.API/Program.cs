d452581585ab247c487753d0d42c7e0170a30c57
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2022-07-02 23:51:34 +0700

    Add project files.

---
+++ src/Services/Hangfire.API/Program.cs
@@ -0,0 +1,50 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Information("Start Hangfire API up");
+
+try
+{
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+}
+
+catch (Exception ex)
+{
+    string type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+    {
+        throw;
+    }
+
+    Log.Fatal(ex, $"Unhandled exception: {ex.Message}");
+}
+finally
+{
+    Log.Information("Shut down Hangfire API complete");
+    Log.CloseAndFlush();
+}
