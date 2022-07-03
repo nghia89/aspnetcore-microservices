e87ce6201227f0cd27f9d799650978959216a027
Nguyen Thanh Nghia <37038823+nghia89@users.noreply.github.com>
2022-07-03 23:06:32 +0700

    add dbcontext repository pattern

---
+++ src/Services/Product.API/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Infrastructure.Mappings;
+using Infrastructure.Shared.DTOs.Product;
+using Product.API.Entities;
+
+namespace Product.API
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CatalogProduct, ProductDto>();
+            CreateMap<CreateProductDto, CatalogProduct>();
+            CreateMap<UpdateProductDto, CatalogProduct>()
+            .IgnoreAllNonExisting();
+        }
+    }
+}
--- src/Services/Product.API/Program.cs
+++ src/Services/Product.API/Program.cs
@@ -1,3 +1,6 @@
+using Common.Logging;
+using Product.API.Extensions;
+using Product.API.Persistence;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,31 +9,21 @@ Log.Information("Start Product API up");
 
 try
 {
- 
-    // Add services to the container.
 
-    builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    builder.Host.UseSerilog(Serilogger.Configure);
+    builder.Host.AddAppConfigurations();
+    // Add services to the container.
+    builder.Services.AddInfrastructure(builder.Configuration);
 
     var app = builder.Build();
+    app.UseInfrastructure();
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
+    app.MigrateDatabase<ProductContext>((context, _) =>
     {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.Run();
-
+        ProductContextSeed.SeedProductAsync(context, Log.Logger).Wait();
+    })
+        .Run();
+ 
 }
 
 catch (Exception ex)
